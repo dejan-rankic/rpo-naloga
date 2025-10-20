c528987097035cf64c733b32b19f4c3d26a971b9
dejan <dejan.rankic@student.um.si>
2025-10-20 20:51:29 +0200

    Dodana osnovna verzija kalkulatorja v C++

---
+++ kalkulator.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+double saberi(double prviBroj, double drugiBroj) {
+    return prviBroj + drugiBroj;
+}
+
+double oduzmi(double prviBroj, double drugiBroj) {
+    return prviBroj - drugiBroj;
+}
+
+double pomnozi(double prviBroj, double drugiBroj) {
+    return prviBroj * drugiBroj;
+}
+
+double podeli(double prviBroj, double drugiBroj) {
+    if (drugiBroj == 0) {
+        cout << "Greska: deljenje sa nulom!" << endl;
+        return 0;
+    }
+    return prviBroj / drugiBroj;
+}
+
+int main() {
+    cout << "Jednostavan kalkulator" << endl;
+    cout << "Izaberi operaciju:" << endl;
+    cout << "1. Sabiranje" << endl;
+    cout << "2. Oduzimanje" << endl;
+    cout << "3. Mnozenje" << endl;
+    cout << "4. Deljenje" << endl;
+
+    int izborOperacije;
+    cout << "Unesi broj operacije (1/2/3/4): ";
+    cin >> izborOperacije;
+
+    double prviBroj, drugiBroj;
+    cout << "Unesi prvi broj: ";
+    cin >> prviBroj;
+    cout << "Unesi drugi broj: ";
+    cin >> drugiBroj;
+
+    switch(izborOperacije) {
+        case 1:
+            cout << "Rezultat: " << saberi(prviBroj, drugiBroj) << endl;
+            break;
+        case 2:
+            cout << "Rezultat: " << oduzmi(prviBroj, drugiBroj) << endl;
+            break;
+        case 3:
+            cout << "Rezultat: " << pomnozi(prviBroj, drugiBroj) << endl;
+            break;
+        case 4:
+            cout << "Rezultat: " << podeli(prviBroj, drugiBroj) << endl;
+            break;
+        default:
+            cout << "Nevalidan izbor!" << endl;
+    }
+
+    return 0;
+}
